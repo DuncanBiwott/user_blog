e564faca4e86ab4683e371ba5914baae292e2594
DuncanBiwott <bduncan19@student.kibu.ac.ke>
2022-09-30 13:54:20 +0300

    changed text fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:post_app/services/auth.dart';
 import 'package:post_app/view/home_page.dart';
 import 'package:post_app/view/login_page.dart';
@@ -9,7 +10,7 @@ import 'package:post_app/view/login_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.green,
-        fontFamily: 'Raleway',
+        textTheme: GoogleFonts.robotoCondensedTextTheme(),
       ),
       home: Root(),
     );
